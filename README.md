a97e9b87b67e8ae37663cc422970184dda40d9c3
okojomoeko <chikoro.size@gmail.com>
2021-06-30 14:33:45 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # renaming-media-files-script
+
+## Description
+
+The script that renames media files.
+
+## Demo
+
+## Requirement
+
+- `exiftool`
+  - `sudo apt install exiftool`
+
+## Usage
+
+example directory
+
+```
+temp
+|--IMG_20210610_191943.jpg
+|--IMG_20210610_191959.jpg
+|--IMG_20210610_191959_1.png
+|--IMG_20210610_191959_2.jpg
+|--mov_hts-samp001.mp4
+```
+
+run script
+
+```
+$ bash ./rename-media.sh ~/temp
+
+/home/vagrant/temp/IMG/2021-06-10/IMG_20210610_191943.jpg
+/home/vagrant/temp/IMG/2021-06-10/IMG_20210610_191959.jpg
+/home/vagrant/temp/IMG/2021-06-10/IMG_20210610_191959_1.png
+/home/vagrant/temp/IMG/2021-06-10/IMG_20210610_191959_2.jpg
+/home/vagrant/temp/VID/2017-09-11/VID_20170911_154935.mp4
+```
+
+after script run
+
+```
+temp
+|--IMG
+|  |--2021-06-10
+|  |  |--IMG_20210610_191943.jpg
+|  |  |--IMG_20210610_191959.jpg
+|  |  |--IMG_20210610_191959_1.png
+|  |  |--IMG_20210610_191959_2.jpg
+|--VID
+|  |--2017-09-11
+|  |  |--VID_20170911_154935.mp4
+```
+
+## Install
+
+## Contribution
+
+## Licence
+
+## Author
+
+[okojomoeko](https://gitlab.com/okojomoeko)
---
+++ rename-media.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Select the directory where the images and videos are located" 1>&2
+  exit 1
+fi
+
+if !(type "exiftool" > /dev/null 2>&1); then
+  echo "exiftool is not found"
+  exit 1
+fi
+
+
+imgregex="jpg|png"
+vidregex="mp4|mov"
+
+filenames=$( find $1 -maxdepth 1 -type f  -regextype posix-egrep -regex ".*($imgregex|$vidregex)" )
+
+count=1
+tempRenamedFile=""
+
+for eachValue in $filenames; do
+  ext=$( echo "$eachValue" | sed -e "s/.*\.//")
+
+  prefix=""
+  if [[ $ext =~ $imgregex ]]; then
+    prefix="IMG"
+  elif [[ $ext =~ $vidregex ]]; then
+    prefix="VID"
+  fi
+  current=$( dirname $eachValue)
+
+  res=$(exiftool -CreateDate "$eachValue")
+
+  timestamp=$( echo "$res" | sed -e "s/.*\([0-9]\{4\}\):\([0-9]\{2\}\):\([0-9]\{2\}\)\s\([0-9]\{2\}\):\([0-9]\{2\}\):\([0-9]\{2\}\).*/\1\2\3_\4\5\6/")
+
+  dirname=$( echo "$timestamp" | sed -e "s/\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\).*/\1-\2-\3/" )
+
+
+  if [ ! -d "$current/$prefix/$dirname" ]; then
+    mkdir -p "$current/$prefix/$dirname"
+  fi
+
+  renamedfile=$(echo "$prefix"_"$timestamp")
+
+  if [ "$renamedfile" = "$tempRenamedFile" ]; then
+    renamedfile="${renamedfile}_${count}.$ext"
+    let count++
+  else
+    tempRenamedFile=$renamedfile
+    renamedfile="${renamedfile}.$ext"
+    count=1
+  fi
+
+  echo "$current/$prefix/$dirname/$renamedfile"
+
+  mv "$eachValue" "$current/$prefix/$dirname/$renamedfile"
+
+done
