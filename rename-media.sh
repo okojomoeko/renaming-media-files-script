2c41cd19aa77dd83e0fb169044ba6bf85cf1670a
okojomoeko <chikoro.size@gmail.com>
2023-01-06 20:58:53 +0900

    コマンドを減らした

--- rename-media.sh
+++ rename-media.sh
@@ -13,9 +13,15 @@ fi
 
 imgregex="jpg|png|hsp"
 vidregex="mp4|mov"
+declare -A prefix
+prefix["jpg"]="IMG"
+prefix["png"]="IMG"
+prefix["hsp"]="IMG"
+prefix["mp4"]="VID"
+prefix["mov"]="VID"
 
-echo $1
-filenames=$( find $1 -maxdepth 1 -type f | grep -i -E ".*($imgregex|$vidregex)" )
+current=$1
+filenames=$( find $current -maxdepth 1 -type f | grep -i -E ".*($imgregex|$vidregex)" )
 
 count=1
 tempRenamedFile=""
@@ -23,38 +29,36 @@ tempRenamedFile=""
 for eachValue in $filenames; do
   ext=$( echo "$eachValue" | sed -e "s/.*\.//")
 
-  prefix=""
-  if [ $( echo $ext | grep -i -E "$imgregex" ) ]; then
-    prefix="IMG"
-  elif [ $( echo $ext | grep -i -E "$vidregex" ) ]; then
-    prefix="VID"
-  fi
-  current=$( dirname $eachValue)
-  # echo $eachValue
-  res=$(exiftool -api QuickTimeUTC -api largefilesupport=1 -CreateDate "$eachValue")
-  # echo $res
+  # prefix=""
+  # if [ $( echo $ext | grep -i -E "$imgregex" ) ]; then
+  #   prefix="IMG"
+  # elif [ $( echo $ext | grep -i -E "$vidregex" ) ]; then
+  #   prefix="VID"
+  # fi
 
-  timestamp=$( echo "$res" | sed -e "s/.*\([0-9]\{4\}\):\([0-9]\{2\}\):\([0-9]\{2\}\) \([0-9]\{2\}\):\([0-9]\{2\}\):\([0-9]\{2\}\).*/\1\2\3_\4\5\6/")
+  # echo $eachValue
+  timestamp=$(exiftool -api QuickTimeUTC -api largefilesupport=1 -CreateDate "$eachValue" | sed -e "s/.*\([0-9]\{4\}\):\([0-9]\{2\}\):\([0-9]\{2\}\) \([0-9]\{2\}\):\([0-9]\{2\}\):\([0-9]\{2\}\).*/\1\2\3_\4\5\6/")
 
   dirname=$( echo "$timestamp" | sed -e "s/\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\).*/\1-\2-\3/" )
 
-  if [ ! -d "$current/$prefix/$dirname" ]; then
-    mkdir -p "$current/$prefix/$dirname"
+  if [ ! -d "$current/${prefix[${ext,,}]}/$dirname" ]; then
+    mkdir -p "$current/${prefix[${ext,,}]}/$dirname"
   fi
 
-  renamedfile=$(echo "$prefix"_"$timestamp")
+  renamedfile="${prefix[${ext,,}]}_${timestamp}"
 
   if [ "$renamedfile" = "$tempRenamedFile" ]; then
-    renamedfile="${renamedfile}_${count}.$ext"
+    renamedfile="${renamedfile}_${count}.${ext}"
     let count++
   else
     tempRenamedFile=$renamedfile
-    renamedfile="${renamedfile}.$ext"
+    renamedfile="$renamedfile.$ext"
     count=1
   fi
 
-  echo "$current/$prefix/$dirname/$renamedfile"
+  echo "$current/${prefix[${ext,,}]}/$dirname/$renamedfile"
+  echo $eachValue
 
-  mv "$eachValue" "$current/$prefix/$dirname/$renamedfile"
+  mv "$eachValue" "$current/${prefix[${ext,,}]}/$dirname/$renamedfile"
 
 done
