4bd9292176fc156a97193e3d51dbbb6f01183af9
okojomoeko <chikoro.size@gmail.com>
2023-10-08 22:04:39 +0900

    add heic

--- rename-media.sh
+++ rename-media.sh
@@ -10,5 +10,5 @@ if !(type "exiftool" > /dev/null 2>&1); then
   exit 1
 fi
 
-exiftool -api QuickTimeUTC -api largefilesupport=1 -execute "-FileName<CreateDate" -d "IMG_%Y%m%d_%H%M%S%%-c.%%e" -ext jpg -ext png -ext+ hsp $1 -execute "-Directory<CreateDate" -d "$1/IMG/%Y-%m-%d" -ext jpg -ext png -ext+ hsp $1;
+exiftool -api QuickTimeUTC -api largefilesupport=1 -execute "-FileName<CreateDate" -d "IMG_%Y%m%d_%H%M%S%%-c.%%e" -ext jpg -ext png -ext+ hsp -ext+ heic $1 -execute "-Directory<CreateDate" -d "$1/IMG/%Y-%m-%d" -ext jpg -ext png -ext+ hsp -ext+ heic $1;
 exiftool -api QuickTimeUTC -api largefilesupport=1 -execute "-FileName<CreateDate" -d "VID_%Y%m%d_%H%M%S%%-c.%%e" -ext MP4 -ext MOV $1 -execute "-Directory<CreateDate" -d "$1/VID/%Y-%m-%d" -ext MP4 -ext MOV $1;
